8f6c6abac29f393f7655a70b2b6058aa202a69cf
pl1ntuc <polina342019@gmail.com>
2025-03-25 14:56:06 +0300

    Add files via upload

---
+++ plow/Новый текстовый документ..py
@@ -0,0 +1,21 @@
+import math
+
+def max_gcd_split(N):
+    max_d = 1
+    for i in range(1, int(math.sqrt(N)) + 1):
+        if N % i == 0:
+            if i <= N // 2:
+                max_d = max(max_d, i)
+            if N // i <= N // 2:
+                max_d = max(max_d, N // i)
+    return max_d, N - max_d
+
+# Чтение входного числа из файла
+with open('input.txt', 'r') as infile:
+    N = int(infile.readline().strip())
+
+A, B = max_gcd_split(N)
+
+# Запись результата в выходной файл
+with open('output.txt', 'w') as outfile:
+    outfile.write(f"{A} {B}\n")
